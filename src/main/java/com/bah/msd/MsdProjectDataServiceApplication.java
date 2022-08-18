bd769eda199c923830a645aa8ae950c8587b315b
Saravanan Kuppusamy <saravanan_kuppusamy@yahoo.com>
2022-08-18 15:31:47 -0400

    data service

---
+++ src/main/java/com/bah/msd/MsdProjectDataServiceApplication.java
@@ -0,0 +1,14 @@
+package com.bah.msd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsdProjectDataServiceApplication {
+
+	public static void main(String[] args) {
+		System.out.println("my rock n roll spring boot app is starting.....all cool..");
+		SpringApplication.run(MsdProjectDataServiceApplication.class, args);
+	}
+
+}
